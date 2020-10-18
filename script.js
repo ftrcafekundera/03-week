135606486593f55b78680f27cc0f9ee8adc8e4e9
ftrcafekundera <71603259+ftrcafekundera@users.noreply.github.com>
2020-10-17 20:33:17 -0400

    Added in comments to the JavaScript

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Array of characters to be included in the password
+//Array of Numbers and characters that an employee can choose from while answering the prompts.
 var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"];
 var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var specialCharacters = [ "+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^",
 "~", "*", "?", ":"];
-// Write password to the #password input
+
+// First prompt that will appear after the employee clicks on Generate Password;
+// Per the IF statement below, the number that the employee inputs must be greater than 7 and less than 129.
 
 function promptUser(){
   var pwdLength = parseInt(
     prompt("How many characters for the password?")
   );
 
+// Questions that will appear as prompts for an employee.
+  
   if(pwdLength > 7 && pwdLength < 129){
     var containsNums = confirm("Do you want numbers in the password?");
     var containsUpper = confirm("Do you want upper-case letters in the password?");
@@ -31,6 +35,8 @@ function promptUser(){
     alert("Password needs to be between 8 and 128 characters.")
   }
 }
+
+// Function that moves the numbers/characters selected by the employee to a new array, which will eventually be used to generate a randomized password.
 
 function generatePassword(){
   var inputs =promptUser()
@@ -58,9 +64,13 @@ function generatePassword(){
     result.push(randomizedArray[i])
   }
   console.log(result.join(''))
+  
+  // Action that will move the password and place it in the center box on the interface.
 
 return result.join('')
 }
+
+// Function that will shuffle the numbers/characters and provide a randomized password.
 
 function shuffle(array){
   var currentIndex = array.length, temporaryValue, randomIndex;
@@ -77,6 +87,8 @@ function shuffle(array){
 
   return array;
 }
+
+// Function used to write the password.
 
 function writePassword() {
   var password = generatePassword();
